b8df3b4de1a62546820c19bb81360f87ddc5d045
Sergey <serhii@peppernode.net>
2024-05-20 15:54:42 +0300

    -- add ignore files;

--- conftest.py
+++ conftest.py
@@ -5,9 +5,10 @@ import pytest
 def xml_data():
     return '<?xml version="1.0" encoding="UTF-8" ?> <root>    <name>pudding</name>    <ingredients>       <item>milk</item>       <quantity>1</quantity>       <unit>gallon</unit>    </ingredients>    <ingredients>       <item>sugar</item>       <quantity>2</quantity>       <unit>cups</unit>    </ingredients>    <ingredients>       <item>vanilla</item>       <quantity>10</quantity>       <unit>ml</unit>    </ingredients>    <ingredients>       <item>egg yolks</item>       <quantity>12</quantity>       <comment>room temperature</comment>       <unit></unit>    </ingredients>    <preperations>omitted for brevity</preperations> </root>'
 
+
 @pytest.fixture
 def yaml_data():
-    return '''---
+    return """---
 name: pudding
 ingredients:
 - item: milk
@@ -24,30 +25,16 @@ ingredients:
   comment: room temperature
 preperations:
 - omitted for brevity
-'''
+"""
+
+
 @pytest.fixture
 def result_ingredients():
     return [
-        {
-            "item": "milk",
-            "quantity": 3.78,
-            "unit": "liter"
-        },
-        {
-            "item": "sugar",
-            "quantity": 480.0,
-            "unit": "ml"
-        },
-        {
-            "item": "vanilla",
-            "quantity": 10.0,
-            "unit": "ml"
-        },
-        {
-            "item": "egg yolks",
-            "quantity": 12.0,
-            "comment": "room temperature"
-        }
+        {"item": "milk", "quantity": 3.78, "unit": "liter"},
+        {"item": "sugar", "quantity": 480.0, "unit": "ml"},
+        {"item": "vanilla", "quantity": 10.0, "unit": "ml"},
+        {"item": "egg yolks", "quantity": 12.0, "comment": "room temperature"},
     ]
 
 
@@ -56,28 +43,10 @@ def result_parse_file():
     return {
         "name": "pudding",
         "ingredients": [
-            {
-                "item": "milk",
-                "quantity": 3.78,
-                "unit": "liter"
-            },
-            {
-                "item": "sugar",
-                "quantity": 480.0,
-                "unit": "ml"
-            },
-            {
-                "item": "vanilla",
-                "quantity": 10.0,
-                "unit": "ml"
-            },
-            {
-                "item": "egg yolks",
-                "quantity": 12.0,
-                "comment": "room temperature"
-            }
+            {"item": "milk", "quantity": 3.78, "unit": "liter"},
+            {"item": "sugar", "quantity": 480.0, "unit": "ml"},
+            {"item": "vanilla", "quantity": 10.0, "unit": "ml"},
+            {"item": "egg yolks", "quantity": 12.0, "comment": "room temperature"},
         ],
-        "preperations": [
-            "omitted for brevity"
-        ]
+        "preperations": ["omitted for brevity"],
     }
